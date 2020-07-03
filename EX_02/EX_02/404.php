1b650c77f0be4682333e331dfc27afe643976699
odpr <67262187+odpr@users.noreply.github.com>
2020-07-03 17:55:33 +0200

    Push 5

--- EX_02/EX_02/404.php
+++ EX_02/EX_02/404.php
@@ -1,4 +1,11 @@
-<?php
-echo "<h1>404</h1>";
-echo '<img src="assets" alt="meme">';
-?>
+<!DOCTYPE html>
+<html>
+    <body>
+<center>
+    <h1>ERROR 404</h1>
+    <img src="pepe-frog.jpg" alt="erreur 404 grenouille trump"
+    width="478"
+    height="410">
+</center>
+    </body>
+</html>
