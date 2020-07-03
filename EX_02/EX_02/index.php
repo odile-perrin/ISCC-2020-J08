89f519501e31fb4ee5ebb8efeedca796425c1f76
odpr <67262187+odpr@users.noreply.github.com>
2020-07-03 12:36:17 +0200

    Push 3

--- EX_02/EX_02/index.php
+++ EX_02/EX_02/index.php
@@ -2,28 +2,36 @@
 <html>
 <header>
     <title>SCC Accueil</title>
-    
-    <?php include ("navigation.php");?>
-
     <div id="blocimage"> 
         <h1>Summer Code Camp</h1>
     </div>      
 
-    <link rel="stylesheet" href="navigation.css">
 <?php $page = $_SERVER['PHP_SELF']; ?>
 
 
 <nav class="menu">
 <ul>
-    <li class="<?php if($page=='accueil'){echo 'active';}?>"><A HREF="vitrine-accueil.php">ACCUEIL</A> 
-    <li class="<?php if($page=='programme'){echo 'active';}?>"><A HREF="vitrine-programme.php">PROGRAMME</A> 
-    <li class="<?php if($page=='contact'){echo 'active';}?>"><A HREF="vitrine-contacts.php">CONTACT</A> 
+    <li class="<?php if($page=='accueil'){echo 'active';}?>"><A HREF="http://localhost/ISCC/J08/EX_02/EX_02/vitrine-accueil.php">ACCUEIL</A> 
+    <li class="<?php if($page=='programme'){echo 'active';}?>"><A HREF="http://localhost/ISCC/J08/EX_02/EX_02/vitrine-programme.php">CONTACT</A> 
+    <li class="<?php if($page=='contact'){echo 'active';}?>"><A HREF="http://localhost/ISCC/J08/EX_02/EX_02/vitrine-contacts.php">CONTACT</A> 
 </ul>
 </nav>
 
 </header>
 
 <body>
+<?php
+    if($_GET['page'] == ACCUEIL){
+   echo '<A HREF="vitrine-accueil.php">ACCUEIL</A>';
+
+}
+    elseif($_GET['page'] == PROGRAMME){
+    echo '<A HREF="vitrine-programme.php">PROGRAMME</A>';
+}
+    elseif ($_GET['page'] == CONTACT){
+    echo '<A HREF="vitrine-contacts.php">CONTACT</A>';
+}
+?>
     <footer>
     <a href="https://www.epitech.eu/digital/">
         <img class="logo" src="assetD04/logo_epitech.png" alt="Logo epitech"></a>
